079480978d5615f8ec2f941115d179619bb8e1b0
Jan Adler <jan.adler@barkhauseninstitut.org>
2026-01-09 11:28:54 +0100

    Hardware loop example

--- hardware_loop.py
+++ hardware_loop.py
@@ -1,34 +1,29 @@
 # -*- coding: utf-8 -*-
 
-from hermespy.hardware_loop import HardwareLoop, UsrpDevice, UsrpSystem, ReceivedConstellationPlot, RadarRangePlot, PhysicalDeviceDummy, PhysicalScenarioDummy
+import matplotlib.pyplot as plt
+
+from hermespy.hardware_loop import HardwareLoop, UsrpDevice, UsrpSystem, ReceivedConstellationPlot, RadarRangePlot, PhysicalDeviceDummy, PhysicalScenarioDummy, DeviceReceptionPlot
 from hermespy.jcas import MatchedFilterJcas
 from hermespy.modem import ReceivingModem
 
 from nr_waveform import NRSubframe
 
-#loop = HardwareLoop[UsrpSystem, UsrpDevice](UsrpSystem())
-loop = HardwareLoop[PhysicalScenarioDummy, PhysicalDeviceDummy](PhysicalScenarioDummy())
-numerology_bandwidths = [12 * 15e3 * 2 ** n for n in range(0, 7)]
-device_params = {
-    'carrier_frequency': 6e9,
-    'bandwidth': numerology_bandwidths[6],
-    'oversampling_factor': 16,
-}
-
-# Add USRP devices
-base_station = loop.new_device(**device_params)
-vehicle = loop.new_device(**device_params)
+# Add USRP device
+loop = HardwareLoop[UsrpSystem, UsrpDevice](UsrpSystem())
+base_station = loop.new_device(ip='10.11.58.81', carrier_frequency=5e8)
 
 # Configure 5G NR-like OFDM waveform
-waveform = NRSubframe()
+waveform = NRSubframe(synchronize=True)
 jcas_dsp = MatchedFilterJcas(200.0, waveform)
 rx_modem = ReceivingModem(waveform=waveform)
-base_station.add_dsp(jcas_dsp)
-vehicle.add_dsp(rx_modem)
+base_station.transmitters.add(jcas_dsp)
+base_station.receivers.add(rx_modem)
+#vehicle.add_dsp(rx_modem)
 
 # Add visualization hooks
-loop.add_plot(ReceivedConstellationPlot(rx_modem, 'Vehicle Rx'))
-loop.add_plot(RadarRangePlot(jcas_dsp, 'BS JCAS Range'))
+# loop.add_plot(ReceivedConstellationPlot(rx_modem, 'Vehicle Rx'))
+# loop.add_plot(DeviceReceptionPlot(base_station, 'BS Reception'))
+# loop.add_plot(RadarRangePlot(jcas_dsp, 'BS JCAS Range'))
 
 loop.num_drops = 100
 loop.run()
